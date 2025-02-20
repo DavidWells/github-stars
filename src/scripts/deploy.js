14685857ad03db52e6b21a0e7c6ed2c4c3910551
DavidWells <hello@davidwells.io>
2025-02-20 15:02:21 -0800

    try api again

--- src/scripts/deploy.js
+++ src/scripts/deploy.js
@@ -1,14 +1,16 @@
-import ghpages from 'gh-pages'
+import ghPages from 'gh-pages'
 import { generateStaticSite } from '../utils/generate-site.js'
 import { SITE_DIRECTORY } from '../_constants.js'
 
 async function buildAndDeploy() {
   /* Generate the static site */
+  console.log('Generating static site...')
   await generateStaticSite()
 
   /* Deploy the static site */
+  console.log('Deploying to gh-pages...')
   try {
-    await ghpages.publish(SITE_DIRECTORY)
+    await ghPages.publish(SITE_DIRECTORY)
     console.log('Successfully deployed to gh-pages')
   } catch (err) {
     console.error('Failed to deploy:', err)
